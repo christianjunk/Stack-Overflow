0208c088a3c07836fdc7644490baaf78345dd37a
Christian Junk <christian.junk@geomatics.io>
2018-01-06 01:53:33 +0100

    Initial Commit

---
+++ src/Q32001621/Q32001621.Tests/TestForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using StackOverflow;
+
+namespace Stackoverflow.Test
+{
+    public partial class TestForm : Form
+    {
+        WinEventDelegate dele = null;
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        public TestForm()
+        {
+            InitializeComponent();
+
+            dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            textBox1.AppendText(GetActiveWindowTitle() + "\n");
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            return UwpUtils.GetProcessName(GetForegroundWindow());
+        }
+    }
+}
