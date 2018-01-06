fe61bd744814c047327edb08e2c8852a5496eb65
Christian Junk <christian.junk@geomatics.io>
2018-01-06 16:44:18 +0100

    Fixed assembly name error.

--- src/Q32001621/Q32001621/Properties/AssemblyInfo.cs
+++ src/Q32001621/Q32001621/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StackOverflow")]
+[assembly: AssemblyTitle("Q32001621")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9ee2c284-07a3-4463-80a5-062471d34bbb")]
+[assembly: Guid("411a25a1-fbbb-4f29-9e04-54e762d5eeb7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
